35742e21411c74a7456c3f1df96ebb9bf838fa73
t0mday <95492549+t0mday@users.noreply.github.com>
2021-12-23 15:50:27 +0000

    codewindow development

--- resources/scripts/codeWindow.js
+++ resources/scripts/codeWindow.js
@@ -2,89 +2,77 @@ const js = document.getElementById("js");
 const py = document.getElementById("py");
 const php = document.getElementById("php");
 const java = document.getElementById("java");
-const code = document.getElementById("code")
+const codeWindow = document.getElementById("codeWindow");
 
-js.addEventListener("mouseover", writeCode);
-py.addEventListener("mouseover", writeCode);
-php.addEventListener("mouseover", writeCode);
-java.addEventListener("mouseover", writeCode);
+js.addEventListener("click", writeCode);
+py.addEventListener("click", writeCode);
+php.addEventListener("click", writeCode);
+java.addEventListener("click", writeCode);
 
-let timeouts = []; // stores references to timeouts so they can be cleared.
+let timer; 
 
 function writeCode(event) {
-    clearCode();
-    typeContent(event.target);
+    clearInterval(timer);
+    const content = selectCode(event.target);
+    let i=0;
+    timer = setInterval(() => {
+        if(i >= content.length) {
+            clearInterval(timer);
+            return;
+        }
+        let codeItem = content.slice(0, i+1).join('');
+        let openTags = codeItem.match(/<span[\w\W]+?>/g);
+        let closedTags = codeItem.match(/<\/span>/g) || [];
+
+        if(openTags.length > closedTags.length) {  
+            codeItem += '</span>';
+        }
+        codeItem = '<p>' + codeItem + '</p>';
+        $('#codeWindow p').replaceWith(codeItem);
+        i++;
+    }, 15);
 }
 
-function clearCode() {
-    for(let t in timeouts) clearTimeout(t);
-    codeWindow.innerHTML = "";
-}
-/* this version works, albeit for some reason the whitespace formatting isn't coming through 
-function typeContent(eventTarget) {
-    const content = formatWhitespace(selectSnippet(eventTarget));
-    content.forEach((l, i) => {//content.split('').forEach((l, i) => {
-        let timeout = setTimeout(() => codeWindow.innerHTML += l, i * 20);
-        timeouts.push(timeout);
-    });
-}
-*/
-function typeContent(eventTarget) {
-    const content = selectSnippet(eventTarget);
-    content.split('').forEach((l, i) => {
-        let timeout = setTimeout(() => {
-            if(l === '\n') l = '<br>';
-            else if(l === ' ') l = '&nbsp;';
-            codeWindow.innerHTML += l;
-        }, i * 10);
-        timeouts.push(timeout);
-    });
-}
-
-function selectSnippet(eventTarget) {
-    switch(eventTarget) {
+function selectCode(lang) {
+    switch(lang) {
         case js: return 0;
-        case py: return pyCode;
+        case py: return formatCode(pyCode);
         case php: return 0;
         case java: return 0;
     }  
 }
 
 const pyCode = 
-`# Python:
-# Deep clone a list
-def deep_clone(list_in):
-    if(type(list_in) == list):
-        return [deep_clone(layer) for layer in list_in]
-    else:
-        return list_in
+`<span class="comment"># Deep clone n-dimensional list</span>
+<span class="fd">def</span> <span class="fn">deep_clone</span>(list_in):
+    <span class="keyword">if</span>(type(list_in) == list):
+        <span class="keyword">return</span> [deep_clone(layer) <span class="keyword">for</span> layer <span class="keyword">in</span> list_in]
+    <span class="keyword">else</span>:
+        <span class="keyword">return</span> list_in
 `;
 
-/* This seems to leave the function ok but not received ok. Perhaps something to 
-do with the asynchronous setTimeout that receives it?
-function formatWhitespace(code) {
-    console.log(code);
-    let formatted = code.split('');
-    formatted.forEach(c => {
-        if(c === '\n') c = "<br>";
-        else if(c === ' ') c = "&nbsp;";
-    });
-    return formatted;
+function formatCode(code) {
+    let formattedArray = [];
+    const spanOpenRegex = /^<span[\w\W]+?>/;
+    const spanCloseRegex = /^<\/span>/;
+    let spanOpen = '';
+    let spanClose = '';
+    for(let i=0; i<code.length; i++) {
+        spanOpen = code.slice(i).match(spanOpenRegex);
+        spanClose = code.slice(i).match(spanCloseRegex);
+        if(spanOpen) {
+            formattedArray.push(spanOpen);
+            i += spanOpen[0].length-1;
+        } else if(spanClose) {
+            formattedArray.push(spanClose);
+            i += spanClose[0].length-1;
+        } else if(code[i] === '\n') {
+            formattedArray.push('<br>');
+        } else if(code[i] === ' ') {
+            formattedArray.push('&nbsp;')
+        }else {
+            formattedArray.push(code[i]);
+        }
+    }
+    return formattedArray;
 }
-*/
-
-const pyCode2 = `<pre class="hljs" style="display: block; overflow-x: auto; padding: 0.5em; background-color: rgb(40, 42, 54); color: rgb(248, 248, 242);"><span class="hljs-comment" style="color: rgb(98, 114, 164);"># Deep clone a list</span>
-<span class="hljs-function"><span class="hljs-keyword" style="color: rgb(255, 121, 198); font-weight: 700;">def</span> <span class="hljs-title" style="color: rgb(241, 250, 140); font-weight: 700;">deep_clone</span><span class="hljs-params">(list_in)</span>:</span>
-    <span class="hljs-keyword" style="color: rgb(139, 233, 253); font-weight: 700;">if</span>(type(list_in) == list):
-        <span class="hljs-keyword" style="color: rgb(139, 233, 253); font-weight: 700;">return</span> [deep_clone(layer) <span class="hljs-keyword" style="color: rgb(139, 233, 253); font-weight: 700;">for</span> layer <span class="hljs-keyword" style="color: rgb(139, 233, 253); font-weight: 700;">in</span> list_in]
-    <span class="hljs-keyword" style="color: rgb(139, 233, 253); font-weight: 700;">else</span>:
-        <span class="hljs-keyword" style="color: rgb(139, 233, 253); font-weight: 700;">return</span> list_in
-</pre>`;
-
-function formatStyles(code) {
-    let formatted = code.replace(/<pre[\w\W]+?>/, '').replace('</pre>', '');
-    let twatted = formatted.replace(/<span[\w\W]+?>/g, '¬').replace(/<\/span>/g, '¬');
-    return twatted;
-}
-codeWindow.innerHTML = formatStyles(pyCode2);
-console.log(formatStyles(pyCode2));
